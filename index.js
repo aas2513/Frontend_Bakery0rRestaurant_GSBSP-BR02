daa7410e7498af9931411fa703d79a1fcdc5dc08
aas2513 <60225147+aas2513@users.noreply.github.com>
2020-07-20 00:28:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    jQuery.fn.carousel.Constructor.TRANSITION_DURATION = 2000  // 2 seconds
+  });
+  
+  (function($){
+	function floatLabel(inputType){
+		$(inputType).each(function(){
+			var $this = $(this);
+			// on focus add cladd active to label
+			$this.focus(function(){
+				$this.next().addClass("active");
+			});
+			//on blur check field and remove class if needed
+			$this.blur(function(){
+				if($this.val() === '' || $this.val() === 'blank'){
+					$this.next().removeClass();
+				}
+			});
+		});
+	}
+	// just add a class of "floatLabel to the input field!"
+	floatLabel(".floatLabel");
+})(jQuery);
